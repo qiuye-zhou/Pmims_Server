89462b23ae5ab59163451b91781f76bc2ec309c8
qiuye <1302580865@qq.com>
2022-07-25 21:44:10 +0800

    解决前端跨域问题

--- Server/apinode/app.js
+++ Server/apinode/app.js
@@ -14,13 +14,17 @@ var http = require('http');
 var server = http.createServer(app);
 
 //解决跨域问题，在加载路由之前使用
-app.all('*', function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
-  res.header("X-Powered-By", ' 3.2.1')
-  res.header("Content-Type", "application/json;charset=utf-8");
-  next();
+app.all('*', function(req, res, next) {
+  // console.log(req.headers.origin)
+  // console.log(req.environ)
+  res.header("Access-Control-Allow-Origin", req.headers.origin);
+  // res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
+  res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Credentials","true");
+  res.header("X-Powered-By",' 3.2.1')
+  if(req.method === "OPTIONS") res.send(200);/*让options请求快速返回*/
+  else  next();
 });
 
 // view engine setup
