34bee8fe7eeb98ab28be48d7954166bff2ef6469
qiuye <1302580865@qq.com>
2022-09-29 22:56:04 +0800

    提交文件接口；下载文件的命名处理
    增加用户参加活动提交文件接口提交文件类型的检验处理；下载文件的命名处理为用户名称

--- Server/apinode/routes/file.js
+++ Server/apinode/routes/file.js
@@ -15,26 +15,48 @@ router.post('/uploadac', multer({
     } else {
         let file = req.file;
         const filetype = file.originalname.split('.')[file.originalname.split('.').length - 1]
-
-        fs.renameSync('./public/uploadac/' + file.filename, './public/uploadac/' + `ac${req.body.activ_id}user${req.body.id}.${filetype}`);  //可以根据喜爱命名方式，更改文件名称
-        res.send({
-            code: 200,
-            msg: '上传成功'
-        })
+        if(filetype == 'xlsx' || filetype == 'docx') {
+          //暂时保存在服务端的临时文件改名为对应用户参加活动的文件名保存在服务端
+          fs.renameSync('./public/uploadac/' + file.filename, './public/uploadac/' + `ac${req.body.activ_id}user${req.body.id}.${filetype}`);  //可以根据喜爱命名方式，更改文件名称
+          res.send({
+              code: 200,
+              msg: '上传成功'
+          })
+        } else {
+          //上传文件不符合类型要求，删除上传暂时保存在服务端的临时文件
+          fs.unlinkSync('./public/uploadac/' + file.filename)
+          res.send({
+            code: 406,
+            msg: '上传文件错误'
+          })
+        }
     }
 })
 
 router.get('/download', (req, res) => {
     const name = req.query.name
+    const filename = req.query.filename
     const file = `public/uploadac/${name}.docx`
     fs.access(file, fs.constants.F_OK, (err) => {
     //   console.log(`${file} ${err ? '不存在' : '存在'}`);
       if(err) {
-        const filePath = path.join(__dirname, '../public/uploadac/' + name + '.xlsx')
-      res.download(filePath)
+        fs.copyFile(`./public/uploadac/${name}.xlsx`, `./public/download/${filename}.xlsx`,(err) => {
+          if(err) {
+            console.log(err);
+          } else {
+            const filePath = path.join(__dirname, '../public/download/' + filename + '.xlsx')
+            res.download(filePath)
+          }
+        })
       } else {
-        const filePath = path.join(__dirname, '../public/uploadac/' + name + '.docx')
-      res.download(filePath)
+        fs.copyFile(`./public/uploadac/${name}.docx`, `./public/download/${filename}.docx`,(err) => {
+          if(err) {
+            console.log(err);
+          } else {
+            const filePath = path.join(__dirname, '../public/download/' + filename + '.docx')
+            res.download(filePath)
+          }
+        })
       }
     });
   })
